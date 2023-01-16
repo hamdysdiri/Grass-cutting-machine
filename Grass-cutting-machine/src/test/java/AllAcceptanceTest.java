688fddc2030380b31e376ee83561dbe733d1a9e2
sdiri hamdy <hamdi.sdiri@gmail.com>
2023-01-16 17:53:11 +0100

    add scenario

--- Grass-cutting-machine/src/test/java/AllAcceptanceTest.java
+++ Grass-cutting-machine/src/test/java/AllAcceptanceTest.java
@@ -1,2 +1,20 @@
+import com.grass.GrassMachine;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AllAcceptanceTest {
+    private String firstLine ="5 5";
+    private String secondLine ="1 2 N";
+    private String ThirdLine ="GAGAGAGAA";
+    private String FourthLine ="GAGAGAGAA";
+
+    @Test
+    public void GivenCommand_WhenVerifyIfMeetThePattern_ThenCommandChecked(){
+        assertThat(new GrassMachine().verifyFirstLineYardCommand(firstLine)).isEqualTo(true);
+        assertThat(new GrassMachine().verifyPosition(secondLine)).isEqualTo(true);
+        assertThat(new GrassMachine().verifyInstructions(ThirdLine)).isEqualTo(true);
+        assertThat(new GrassMachine().verifyPosition(secondLine)).isEqualTo(true);
+        assertThat(new GrassMachine().verifyInstructions(FourthLine)).isEqualTo(true);
+    }
 }
