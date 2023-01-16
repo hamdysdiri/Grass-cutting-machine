997c2835fc50d4d12c0f9a0302e3b9d1ea30f6c8
sdiri hamdy <hamdi.sdiri@gmail.com>
2023-01-16 16:46:23 +0100

    green test expect string starts with four digit separated by space

--- Grass-cutting-machine/src/main/java/com/grass/GrassMachine.java
+++ Grass-cutting-machine/src/main/java/com/grass/GrassMachine.java
@@ -7,7 +7,7 @@ public class GrassMachine {
     }
 
     public boolean test(String command) {
-        Pattern p = Pattern.compile("\\d\\s\\d.*");
+        Pattern p = Pattern.compile("\\d\\s\\d\\s\\d\\s\\d.*");
         return Pattern.matches(String.valueOf(p), command) ;
     }
 }
--- Grass-cutting-machine/src/test/java/unit/GrassMachineTest.java
+++ Grass-cutting-machine/src/test/java/unit/GrassMachineTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GrassMachineTest {
-    private String command ="3 3 qsdqsdqsqsd";
+    private String command ="3 3 1 2 qsdqsdqsqsd";
 
     @Test
     public void shouldCommand(){
@@ -14,7 +14,8 @@ public class GrassMachineTest {
     }
 
     @Test
-    public void shouldCommandStartWIIthTwoDigitsSeparatedByWhiteSpace(){
+    public void shouldCommandStartWithFourDigitsSeparatedByWhiteSpace(){
         assertThat(new GrassMachine().test(command)).isEqualTo(true);
     }
+
 }
