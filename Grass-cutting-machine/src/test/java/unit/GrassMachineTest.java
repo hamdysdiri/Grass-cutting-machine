80ae0e657de5f626daaea08dde264a8c0679e142
sdiri hamdy <hamdi.sdiri@gmail.com>
2023-01-16 16:00:52 +0100

    red test

--- Grass-cutting-machine/src/test/java/unit/GrassMachineTest.java
+++ Grass-cutting-machine/src/test/java/unit/GrassMachineTest.java
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test;
 public class GrassMachineTest {
     @Test
     public void shouldCommand(){
-        new GrassMachin().command();
+        new GrassMachin().command("X");
     }
 }
