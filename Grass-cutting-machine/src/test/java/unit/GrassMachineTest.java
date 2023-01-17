423547dec41067cf52f63f5b05c42968854b4d67
sdiri hamdy <hamdi.sdiri@gmail.com>
2023-01-17 13:57:08 +0100

    add throwable check test

--- Grass-cutting-machine/src/test/java/unit/GrassMachineTest.java
+++ Grass-cutting-machine/src/test/java/unit/GrassMachineTest.java
@@ -5,6 +5,8 @@ import com.grass.Yard;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GrassMachineTest {
 
@@ -14,6 +16,7 @@ public class GrassMachineTest {
 
     private Yard yard = new Yard(yardCommandSize);
     private GrassMachine grassMachine = new GrassMachine(yard);
+
     @Test
     public void shouldCommandHavePatternForPosition(){
         assertThat(new GrassMachine(new Yard(yardCommandSize)).verifyPosition(positionLine)).isEqualTo(true);
@@ -30,5 +33,21 @@ public class GrassMachineTest {
         GrassMachine expected = new GrassMachine(new Yard("2 2"));
         expected.putOnPosition("1 1 N");
         assertThat(grassMachine.hashCode()).isEqualTo(expected.hashCode());
+        Exception thrown = assertThrows(
+                Exception.class,
+                () -> expected.putOnPosition("3 3 N"),
+                "Expected doThing() to throw"
+        );
+        assertTrue(thrown.getMessage().contentEquals("error, machine grass will be off the yard"));
+
+    }
+
+
+    @Test()
+    public void shouldGrassMachineBeMove() throws Exception {
+
+        grassMachine.putOnPosition(positionLine);
+
+
     }
 }
